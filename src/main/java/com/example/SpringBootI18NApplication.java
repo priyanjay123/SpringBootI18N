f4ec0df564813fb7ea95e8021e09dd1e2092e4fe
priyanjay goswami <priyanjay.goswami150193@gmail.com>
2019-08-28 10:32:25 +0530

    Added files

---
+++ src/main/java/com/example/SpringBootI18NApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootI18NApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootI18NApplication.class, args);
+	}
+
+}
